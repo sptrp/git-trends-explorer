1d67cbc1b1ed85a68af9babe3b1bd2051a2d345c
i.ponomarev <i.ponomarev@thalia.de>
2021-04-26 17:55:34 +0200

    delete obsolete dagger modules

--- startpage/src/main/java/com/ivanponomarev/data/ReposDataSource.kt
+++ startpage/src/main/java/com/ivanponomarev/data/ReposDataSource.kt
@@ -1,9 +1,10 @@
 package com.ivanponomarev.data
 
 import com.ivanponomarev.domain.Repo
+import dagger.Reusable
 import retrofit2.http.GET
 
-
+@Reusable
 interface ReposDataSource {
     @GET("/")
     suspend fun getAllRepos() : List<Repo>
--- startpage/src/main/java/com/ivanponomarev/data/Repository.kt
+++ startpage/src/main/java/com/ivanponomarev/data/Repository.kt
@@ -1,8 +1,10 @@
 package com.ivanponomarev.data
 
+import dagger.Reusable
 import dagger.hilt.InstallIn
 import javax.inject.Inject
 
+@Reusable
 class Repository @Inject constructor (private val reposDataSource: ReposDataSource) {
 
     suspend fun getAllRepos() = reposDataSource.getAllRepos()
